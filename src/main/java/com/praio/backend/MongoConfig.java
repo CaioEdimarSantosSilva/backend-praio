f36fe7215cea65ba176b7f7b0fa64a1c7d10a091
Caio Silva <136451281+CaioEdimarSantosSilva@users.noreply.github.com>
2026-04-17 17:58:28 -0300

    Configuração MongoDB via .env - senha protegida

--- src/main/java/com/praio/backend/MongoConfig.java
+++ src/main/java/com/praio/backend/MongoConfig.java
@@ -4,20 +4,30 @@ import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
+import java.io.FileInputStream;
+import java.util.Properties;
+
 @Configuration
 public class MongoConfig {
 
-    @Value("${mongodb.uri}")
-    private String mongoUri;
+    private String loadMongoUri() {
+        try {
+            Properties props = new Properties();
+            props.load(new FileInputStream(".env"));
+            return props.getProperty("MONGODB_URI");
+        } catch (Exception e) {
+            throw new RuntimeException("Não foi possível carregar o .env", e);
+        }
+    }
 
     @Bean
     public MongoClient mongoClient() {
-        ConnectionString connectionString = new ConnectionString(mongoUri);
+        String uri = loadMongoUri();
+        ConnectionString connectionString = new ConnectionString(uri);
         MongoClientSettings settings = MongoClientSettings.builder()
                 .applyConnectionString(connectionString)
                 .build();
